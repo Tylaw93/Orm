f58c1a8ac943c8c395b511f327b4742d3fc210e3
Tyler Lawson <tylaw93@yahoo.com>
2021-08-31 14:22:34 -0500

    Added routes to find all products and one product

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,15 +4,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const productInfo = await Product.findAll({
+      include:[{model: Category}, {model: Tag}]
+    });
+    res.status(200).json(productInfo);
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const productInfo = await Product.findByPk(
+      req.params.id, {
+        include:[{model: Category}, {model: Tag}]
+      });
+      res.status(200).json(productInfo);
+    } catch(err){
+      res.status(500).json(err);
+  }
+
 });
 
 // create new product
@@ -89,7 +107,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id' (req, res) => {
   // delete one product by its `id` value
 });
 
